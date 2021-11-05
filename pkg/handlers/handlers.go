729c63d6e04f53dbbc0a54a368ed121f8699abf5
Leonard Ladva <leonardladva@Leonards-MacBook-Pro.local>
2021-11-05 13:13:10 +0200

    Templates converted, working on getting working forms

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/leonard-ladva/bookings/pkg/config"
@@ -33,7 +36,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
@@ -46,7 +49,60 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringmap["remote_ip"] = remoteIP
 
 	// Send the data to the template
-	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.html", &models.TemplateData{
 		StringMap: stringmap,
 	})
+}
+
+// Reservation renders the make a reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.html", &models.TemplateData{})
+}
+
+// Deluxe renders the deluxe suite page
+func (m *Repository) Deluxe(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "deluxe.page.html", &models.TemplateData{})
+}
+
+// Family renders the family room page
+func (m *Repository) Family(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "family.page.html", &models.TemplateData{})
+}
+
+// Availability renders the search availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.html", &models.TemplateData{})
+}
+
+// PostAvailability renders the search availability page
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start, end := r.Form.Get("start"), r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and sends JSON request
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      false,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+// Concatct renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.html", &models.TemplateData{})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
 	"github.com/leonard-ladva/bookings/pkg/config"
 	"github.com/leonard-ladva/bookings/pkg/models"
 )
@@ -16,7 +17,8 @@ var functions = template.FuncMap{}
 
 var app *config.AppConfig
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
@@ -26,7 +28,7 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 // RenderTemplate renders an html template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -42,6 +44,8 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
